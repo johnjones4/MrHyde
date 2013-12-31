a98b27a58ee3e725f6f3148f1c4f03735235bbb8
John Jones <johnjones4@gmail.com>
2013-12-30 20:29:21 -0500

    Initial Commit

---
+++ Mr Hyde/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Mr Hyde
+//
+//  Created by John Jones on 12/30/13.
+//  Copyright (c) 2013 Phoenix4. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
---
+++ Mr HydeImporter/MySpotlightImporter.h
@@ -0,0 +1,19 @@
+//
+//  MySpotlightImporter.h
+//  Mr HydeImporter
+//
+//  Created by John Jones on 12/30/13.
+//  Copyright (c) 2013 Phoenix4. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MySpotlightImporter : NSObject
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (BOOL)importFileAtPath:(NSString *)filePath attributes:(NSMutableDictionary *)attributes error:(NSError **)error;
+
+@end
